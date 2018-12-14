06a9e7925e3f17a7b7ded05d729f0d4edb948805
johnny <johnny@DESKTOP-VVF210T>
2018-12-14 16:30:48 +0800

    02

--- src/main/java/com/business/iservice/ISysMenuBll.java
+++ src/main/java/com/business/iservice/ISysMenuBll.java
@@ -4,6 +4,9 @@ import java.util.List;
 import model.sysmodel.entity.Sys_Menu;
 import model.viewmodel.entity.MenuModel;
 
+/*
+ *       菜单操作
+ */
 public interface ISysMenuBll {
 	/*
 	 *      获取父节点
